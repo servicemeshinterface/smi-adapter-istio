f59e5ce4469aaf1220353a3c858916a7820e8502
Alban Crequy <alban@kinvolk.io>
2019-05-02 16:02:51 +0200

    Register Istio type VirtualService
    operator-sdk add api --api-version=networking.istio.io/v1alpha3 --kind=VirtualService

---
+++ pkg/apis/networking/v1alpha3/virtualservice_types.go
@@ -0,0 +1,49 @@
+package v1alpha3
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// VirtualServiceSpec defines the desired state of VirtualService
+// +k8s:openapi-gen=true
+type VirtualServiceSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// VirtualServiceStatus defines the observed state of VirtualService
+// +k8s:openapi-gen=true
+type VirtualServiceStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// VirtualService is the Schema for the virtualservices API
+// +k8s:openapi-gen=true
+type VirtualService struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   VirtualServiceSpec   `json:"spec,omitempty"`
+	Status VirtualServiceStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// VirtualServiceList contains a list of VirtualService
+type VirtualServiceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []VirtualService `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&VirtualService{}, &VirtualServiceList{})
+}
