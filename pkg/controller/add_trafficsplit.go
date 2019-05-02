88163677e61b21da1dc16c73442df73865c9f6db
Alban Crequy <alban@kinvolk.io>
2019-05-02 11:00:27 +0200

    Add controller for TrafficSplit
    Commit generated with:
    operator-sdk add controller --api-version=smi-spec.io/v1beta1 --kind=TrafficSplit

---
+++ pkg/controller/add_trafficsplit.go
@@ -0,0 +1,10 @@
+package controller
+
+import (
+	"github.com/kinvolk/smi-adapter-istio/pkg/controller/trafficsplit"
+)
+
+func init() {
+	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
+	AddToManagerFuncs = append(AddToManagerFuncs, trafficsplit.Add)
+}
