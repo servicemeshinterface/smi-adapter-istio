eb5f079ff9ccd973d967e57695875c10d3ec877b
Michelle Noorali <michellemolu@gmail.com>
2019-08-08 16:42:39 -0400

    add missing lib to vendor/ + update dep files

--- vendor/github.com/deislabs/smi-sdk-go/pkg/apis/specs/v1alpha1/register.go
+++ vendor/github.com/deislabs/smi-sdk-go/pkg/apis/specs/v1alpha1/register.go
@@ -41,7 +41,6 @@ var (
 func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
 		&HTTPRouteGroup{},
-		&HTTPRouteGroupList{},
 		&TCPRoute{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
